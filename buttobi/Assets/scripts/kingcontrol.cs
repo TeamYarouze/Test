0ddf01c28cb253f31a69f31fb3c501d89cd54387
SyuzoInada <moyasin2006@gmail.com>
2017-03-18 22:34:11 +0900

    いろいろと使えそうなフリーのアセット入れました。あと、JokerScript入れました。 チュートリアルとかでつかるかもしれません。いらないものはどんどん捨ててOKです。

--- buttobi/Assets/scripts/kingcontrol.cs
+++ buttobi/Assets/scripts/kingcontrol.cs
@@ -7,13 +7,15 @@ public class kingcontrol : MonoBehaviour , IPointerClickHandler
 {
 
     Rigidbody rigidBody;
-    public Vector3 force = new Vector3(0, 10, 0);
+    public Vector3 force = new Vector3(0, 200.0f, 0);
     public ForceMode forceMode = ForceMode.VelocityChange;
 
     // Use this for initialization
     void Start () {
         //rigidbodyを取得する。
         rigidBody = gameObject.GetComponent<Rigidbody>();
+        rigidBody.mass = 0.05f;
+        Physics.gravity = new Vector3(0, -20.0f, 0);
     }
 	
 	// Update is called once per frame
